6cac1093fd5dd19bf8415d7c349be27f385233f2
GulajavaMinistudio <gulajava.mini@gmail.com>
2019-12-23 10:31:03 +0700

    Coret coret template literal

---
+++ src/templateliterals.js
@@ -0,0 +1,29 @@
+// const dataTest = 'Hello template';
+// const templateData = `Saya berkata ${dataTest}`;
+// console.log(templateData);
+
+// const lols1 = `he says : "Lol lol lol"`;
+// console.log(lols1);
+
+// let nilai = 10;
+// nilai += 1;
+// console.log(nilai);
+
+// const backslashs = '"hello \n world" \n spasi ke bawah';
+// console.log(backslashs);
+// const backslash2 = '\t hello world';
+// console.log(backslash2);
+// const backslashgabungan = `gabungan dari ${backslashs} dan ${backslash2}`;
+// console.log(backslashgabungan);
+
+// const itemSayur = 'timun';
+// const hargaSayur = 2000;
+// const jumlahSayur = 4;
+// const stringHasilHarga = `Harga total pembelian ${itemSayur} adalah ${hargaSayur *
+//   jumlahSayur}`;
+
+// console.log(stringHasilHarga);
+
+const waktuTes = 5;
+const pesanKirim = `Dikirim sejak ${waktuTes} menit yang lalu`;
+console.log(pesanKirim);
